076a12dd45d2a786941fd303b6eab96c5488d98e
Coddo <coddobusiness@gmail.com>
2022-02-26 11:51:58 +0400

    Done :(((

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
   content: [
     './templates/*.html',
+    './templates/Info.html',
     './static/js/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sand: {
+          100: '#fff2e8',
+          200: '#dd8c8a',
+          300: '#c3875d'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
