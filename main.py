77017707e89e2b070bec9702139f9e028927a4a4
Coddo <coddobusiness@gmail.com>
2022-02-26 11:51:51 +0400

    Done :(((

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 
+@app.route("/")
+def home():
+    return render_template('Home.html')
+
+
 @app.route("/quiz")
 def quiz():
-    return render_template('quiz.html')
+    return render_template('Quiz.html')
+
+
+@app.route("/info")
+def info():
+    return render_template('Info.html')
 
 
 if __name__ == '__main__':
