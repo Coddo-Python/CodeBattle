51982bfa887eb3558be8ede9cac660f73c37308f
Coddo <coddobusiness@gmail.com>
2022-02-26 10:53:53 +0400

    Add `tailwind.config.js`

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+      './templates/*.html',
+      './static/js/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
